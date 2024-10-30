c0802c21e24f3b228f711719747cc4a6c55c416a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-10-30 15:32:29 +0100

    refactor: implement CQRS

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -7,13 +8,13 @@ public record CreateProductCommand(
     List<string> Category,
     string Description,
     string ImageFile,
-    decimal Price) : IRequest<CreateProductResult>;
+    decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
