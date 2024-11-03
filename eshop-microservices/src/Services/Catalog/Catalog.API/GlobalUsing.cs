e13ba66cfe86a262ce53f353182d05ea252525c6
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-03 17:39:14 +0100

    feat: get products endpoint

--- eshop-microservices/src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,6 +1,6 @@
-﻿global using Carter;
-global using Mapster;
-global using MediatR;
-global using BuildingBlocks.CQRS;
+﻿global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
+global using MediatR;
+global using Mapster;
 global using Marten;
+global using Carter;
