6f278822b7d4443e199786baeffc685864f26ead
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-05 07:53:01 +0100

    feat: register and implement validation

--- eshop-microservices/src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,6 +1,7 @@
 ﻿global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
+global using FluentValidation;
 global using MediatR;
 global using Mapster;
 global using Marten;
