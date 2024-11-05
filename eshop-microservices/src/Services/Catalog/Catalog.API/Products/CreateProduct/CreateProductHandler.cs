512f37c14346cc63946231492f0b25fd7cb3ab2d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-05 08:16:14 +0100

    refactor: utilize validation behaviour

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,12 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
 internal class CreateProductCommandHandler
-    (IDocumentSession session, IValidator<CreateProductCommand> validator) 
+    (IDocumentSession session, ILogger<CreateProductCommandHandler> logger) 
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        var result = await validator.ValidateAsync(command, cancellationToken);
-        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-
-        if (errors.Any())
-            throw new ValidationException(errors.FirstOrDefault());
+        logger.LogInformation("CreateProductsCommandHandler.Handle called with {@Command}", command);
 
         var product = new Product
         {
