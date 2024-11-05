73dabffad60d52603f9c3d388acecd3b1cbc3737
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-05 08:22:53 +0100

    feat: add validation to delete and update product

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -15,3 +15,11 @@ internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<Del
 
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+    }
+}
--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Catalog.API.Products.UpdateProduct;
+﻿using System.Data;
+
+namespace Catalog.API.Products.UpdateProduct;
 
 internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
@@ -32,3 +34,16 @@ public record UpdateProductCommand(
     decimal Price) : ICommand<UpdateProductResult>;
 
 public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
