d191ae4f13b45b8f5e9ede56a683b1914dfc9008
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-05 22:02:17 +0100

    feat: implement get basket endpoint

--- eshop-microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -1,6 +1,25 @@
-﻿namespace Basket.API.Basket.GetBasket
+﻿namespace Basket.API.Basket.GetBasket;
+
+public class GetBasketEndpoints : ICarterModule
 {
-    public class GetBasketEndpoints
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/basket/{userName}",
+                async (string userName, ISender sender) =>
+                {
+                    var result = await sender.Send(new GetBasketQuery(userName));
+
+                    var response = result.Adapt<GetBasketResponse>();
+
+                    return Results.Ok(response);
+                })
+            .WithName("GetProductById")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
     }
 }
+
+//public record GetBasketRequest(string userName);
+public record GetBasketResponse(ShoppingCart Cart);
