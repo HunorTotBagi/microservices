b3e75ab4460ff8f9324c4f0e126b3c1075576568
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-05 12:40:28 +0100

    feat: implement health checks

--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
@@ -23,11 +24,21 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+
 app.MapCarter();
 
 app.UseExceptionHandler(_ => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
