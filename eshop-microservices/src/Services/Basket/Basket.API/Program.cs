8203593818ef39e71c48f46c3710bb3c84ff8a1d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-10 20:46:18 +0100

    fix: add missing `;`

--- eshop-microservices/src/Services/Basket/Basket.API/Program.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Program.cs
@@ -37,7 +37,7 @@ builder.Services.AddStackExchangeRedisCache(options =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
-})
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
