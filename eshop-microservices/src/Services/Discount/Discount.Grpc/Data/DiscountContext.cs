3668546af45faee1c2414150522373fcd00a8259
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-10 12:37:25 +0100

    feat: add `DiscountContext.cs`

---
+++ eshop-microservices/src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+	public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+	{
+
+	}
+}
