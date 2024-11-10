065d9201cb83e9e9da2218e663ab2f356552b1e9
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-10 14:21:24 +0100

    feat: get discount

--- eshop-microservices/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ eshop-microservices/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,13 +1,20 @@
-﻿using Grpc.Core;
+﻿namespace Discount.Grpc.Services;
 
-namespace Discount.Grpc.Services;
-
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger) : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        // TODO: GetDiscount from Database
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon is null)
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+        logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+        var couponModel = coupon.Adapt<CouponModel>();
+
+        return couponModel;
     }
 
     public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
