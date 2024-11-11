845b2127b9faaf38d3e05d9055c6510b9dfae641
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-11 14:01:17 +0100

    feat: configure dependency injection

---
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        //services.AddMediatoR(cfg => {
+        //      cfg.RegisterServicesFromAssembly(Assembly.GetExectuingAssemlby());
+        //});
+
+        return services;
+    }
+}
