cae5a88dec45515754f2d2df752d6838432bd5d3
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-23 16:16:41 +0100

    feat: data seeding

--- eshop-microservices/src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -12,5 +12,23 @@ public static class DatabaseExtensions
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedAsync(context);
+    }
+
+    private static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomerAsync(context);
+        //await SeedProductAsync(context);
+        //await SeedOrderandItemsAsync(context);
+    }
+
+    private static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
     }
 }
