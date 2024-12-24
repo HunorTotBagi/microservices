909e717bd20665699cfc3e6d3a2fc4cd2ab2ff79
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-24 22:59:30 +0100

    feat: add dispatch domain events

--- eshop-microservices/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure;
@@ -9,10 +10,14 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        //// Add services to the container
-        services.AddDbContext<ApplicationDbContext>(options =>
+        // Add services to the container
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
-            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.AddInterceptors(sp.GetService<ISaveChangesInterceptor>());
             options.UseSqlServer(connectionString);
         });
 
