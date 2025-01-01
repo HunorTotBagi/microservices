e0938c302930d0a6d72f8976032488c1cc30cd7c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-01 17:14:12 +0100

    feat: implement delete order endpoint

--- eshop-microservices/src/Services/Ordering/Ordering.Api/Endpoints/DeleteOrder.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Api/Endpoints/DeleteOrder.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.Api.Endpoints
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints;
+
+public class DeleteOrder : ICarterModule
 {
-    public class DeleteOrder
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteOrderCommand(Id));
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order");
     }
 }
+
+//public record DeleteOrderRequest(Guid Id);
+public record DeleteOrderResponse(bool IsSuccess);
