e81dfd70038987ba3e374f43e02b460e8b09f25c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-01 19:43:30 +0200

    Section 17: Ordering.API Layer Exposing Minimal Api Endpoints with Carter and REPR Pattern (#11)
    * feat: add `Carter` to `Ordering.Api`
    
    * feat: add endpoints folder and files
    
    * feat: implement create order endpoint
    
    * feat: implement update order
    
    * feat: implement delete order endpoint
    
    * feat: implement get orders by name endpoint
    
    * feat: implement get orders by customer endpoint
    
    * feat: implement get orders endpoint
    
    * feat: register custom exception handling
    
    * feat: improve health checks

--- eshop-microservices/src/Services/Ordering/Ordering.Api/GlobalUsing.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Api/GlobalUsing.cs
@@ -1,3 +1,7 @@
 ﻿global using Ordering.Api;
 global using Ordering.Application;
 global using Ordering.Infrastructure;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
