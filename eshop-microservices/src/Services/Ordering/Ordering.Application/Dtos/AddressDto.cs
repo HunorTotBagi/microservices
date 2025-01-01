e4d87680b00f94f5135aacc5311536474f4daecd
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-01 09:58:40 +0100

    feat: develop `Address`, `Order`, `OrderItem` and `Payment` DTO

---
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record AddressDto(
+    string FirstName,
+    string Lastname,
+    string EmailAddress,
+    string AddressLine,
+    string Country,
+    string State,
+    string ZipCode);
